1b92fdbbcfb9404fa188b25ad37af047fc74c48b
Ashraf <a.ashrafjfr@gmail.com>
2024-04-07 03:42:18 +0800

    Edited files for Variation 17 deployment

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -29,48 +29,35 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const [isIconPlus, setIsIconPlus] = useState(true);
 
   const handleAddToCartClick = () => {
+    setIsClicked(true);
+    toast.success('Item added to cart!', {
+      position: 'top-right',
+      autoClose: 2000,
+    });
     setIsClicked(!isClicked);
     setIsIconPlus(!isIconPlus);
-    if (!isClicked) {
-      toast.success('Item added to cart!', {
-        position: 'top-right',
-        autoClose: 2000,
-      });
-    }
   };
 
-  // Modular Product Card
+  // Row Product Card
   return (
-    <div className="relative w-60 border border-gray-200 rounded-lg shadow-sm p-4">
+    <div className="flex flex-row items-center gap-8 relative w-110 border border-gray-200 rounded-lg shadow-sm p-4">
       <p className="text-sm text-gray-600 mb-2 font-semibold">{product.id}</p>
       <div className="relative">
         <img
           src={product.image}
           alt={product.title}
           className="w-full h-64 object-cover mb-2 rounded-2xl"
-          style={{ maxWidth: '205px', maxHeight: '205px' }}
+          style={{ width: '210px', height: '150px' }}
         />
-        <button 
-          className={`absolute top-3 right-3 focus:outline-none ${
-            isClicked ? 'clicked' : ''
-          }`}
-          onClick={handleAddToCartClick}
-          >
-          {isIconPlus ? (
-            <PlusCircleIcon id="addToCart-button" className='h-10 w-10' fill='#e0e0de'/>
-          ) : (
-            <CheckCircleIcon id="removeFromCart-button" className='h-10 w-10' fill='#2BA41D' />
-          )}
-        </button>
       </div>
-      <h2 className="text-sm font-semibold mb-1">{product.title}</h2>
-      <p className="text-[0.7em] text-gray-600 mb-2">{truncatedDescription}</p>
-      <div className="flex flex-wrap">
-        <div className="w-3/4 pr-2 border-r border-gray-200">
+      <div className="flex flex-col gap-2 w-80 mr-5">
+        <h2 className="text-md font-bold mb-1">{product.title}</h2>
+        <p className="text-[0.8em] text-gray-600 mb-2">{truncatedDescription}</p>
+        <div className="w-3/4">
           <p className="text-[0.6em] text-gray-600">{formattedLocation}</p>
         </div>
         <div className="w-1/4">
-          <p className="text-[0.6em] text-green-400 ml-2">
+          <p className="text-[0.6em] text-green-400">
             {product.delivery ? 'Delivery Covered' : 'Delivery Not Covered'}
           </p>
         </div>
@@ -80,6 +67,21 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           transform: translateY(2px);
         }
       `}</style>
+      <div className='items-center'>
+        <button
+          id="addToCart-text-button"
+          className={`flex flex-row gap-3 justify-center items-center w-40
+          ${isClicked ? 'bg-[#2BA41D] hover:bg-[#217318] text-gray-50' : 'bg-neutral-300 hover:bg-neutral-400 text-gray-50'} text-[0.8em] font-semibold py-2 px-4 rounded-full ml-3 h-10 focus:outline-none`}
+          onClick={handleAddToCartClick}
+          >
+          {isClicked ? (
+            <CheckCircleIcon className='h-4 w-4' fill='#f9fafb'/>
+          ) : (
+            <ShoppingCartIcon className='h-4 w-4' fill='#f9fafb'/>
+          )}
+          {isClicked ? 'Added to Cart' : 'Add To Cart'}
+        </button>
+      </div>
     </div>
   );
 };
